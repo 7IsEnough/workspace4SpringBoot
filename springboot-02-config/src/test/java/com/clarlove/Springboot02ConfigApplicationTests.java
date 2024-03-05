1d902b21cb62fb29701e4d4c996a836c5f2ae74c
Chen Zhenhuan <976949689@qq.com>
2024-03-05 23:10:45 +0800

    2024年3月5日23:10:40

--- springboot-02-config/src/test/java/com/clarlove/Springboot02ConfigApplicationTests.java
+++ springboot-02-config/src/test/java/com/clarlove/Springboot02ConfigApplicationTests.java
@@ -1,13 +1,26 @@
 package com.clarlove;
 
+import com.clarlove.pojo.Dog;
+import com.clarlove.pojo.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot02ConfigApplicationTests {
 
+  @Autowired
+  private Dog dog;
+
+  @Autowired
+  private Person person;
+
   @Test
   void contextLoads() {
+
+    System.out.println(dog);
+
+    System.out.println(person);
   }
 
 }

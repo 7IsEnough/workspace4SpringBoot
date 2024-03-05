19548d7a1e2812415a8ce585613b235736880651
Chen Zhenhuan <976949689@qq.com>
2024-03-05 22:11:08 +0800

    2024年3月5日22:11:01

--- springboot-01-helloworld/src/main/java/com/clarlove/Springboot01HelloworldApplication.java
+++ springboot-01-helloworld/src/main/java/com/clarlove/Springboot01HelloworldApplication.java
@@ -3,10 +3,13 @@ package com.clarlove;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 这个注解标注这个类是一个SpringBoot的应用
+// 启动类下的所有资源被导入
 @SpringBootApplication
 public class Springboot01HelloworldApplication {
 
   public static void main(String[] args) {
+    // 将SpringBoot应用启动
     SpringApplication.run(Springboot01HelloworldApplication.class, args);
   }
 

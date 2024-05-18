275116d17eaa9b4c08554553fc5cbc5d5e7d0acf
Chen Zhenhuan <976949689@qq.com>
2024-05-18 16:37:48 +0800

    2024年5月18日16:37:43

---
+++ dubbo_zookeeper/consumer-server/src/main/java/com/clearlove/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.clearlove.controller;
+
+import com.clearlove.service.TicketServer;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author promise
+ * @date 2024/5/18 - 16:02
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  // 想拿到provider-server提供的票，要去注册中心拿到服务
+  @Reference  //引用 定义路径相同的接口名
+  TicketServer ticketServer;
+
+  @GetMapping("/test")
+  public void buyTicket() {
+    String ticket = ticketServer.getTicket();
+    System.out.println("在注册中心拿到：" + ticket);
+  }
+}
